1e80532261e01a93d398ff3c4455c40df2e05740
joel Busquets <jbusquet@bsc.es>
2026-09-06 03:29:21 +0200

    Eliminar carpeta target subida por error

--- nas/target/debug/deps/onur-161d63e413c9db21.d
+++
@@ -1,5 +0,0 @@
-/Users/joelbusquets/projects/commandos/onur/target/debug/deps/onur-161d63e413c9db21.d: src/main.rs
-
-/Users/joelbusquets/projects/commandos/onur/target/debug/deps/onur-161d63e413c9db21: src/main.rs
-
-src/main.rs:
